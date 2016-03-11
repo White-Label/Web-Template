db4c1a37dc66d48eaa294a017f14bc65fdc74576
Clark Dinnison <cdinnison@gmail.com>
2016-03-11 11:15:52 -0800

    for artist tweaks and club image change

--- source/js/app/misc.js
+++ source/js/app/misc.js
@@ -10,10 +10,10 @@
 // Twitter Follow
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
+// Hide modal on submit form
 $('#signup_form').on('submit', function() {
   $('#myModal').modal('hide');
 });
-
 
 // poster frame click event
 $(document).on('click','.js-videoPoster',function(ev) {
